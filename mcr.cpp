2042fc1716e9c64ae2173600959ebb0b9bb5fe59
crapper <crapper.612@gmail.com>
2023-09-11 20:48:49 +0800

    fix: debug mcr.cpp

--- mcr.cpp
+++ mcr.cpp
@@ -1,5 +1,9 @@
+#include <iostream>
+using namespace std;
+
 bool isWin(char game[3][3]){
     bool win = false;
+    // row
     if (game[0][0] == game[0][1] && game[0][1] == game[0][2] && (game[0][0] == 'X' || game[0][0] == 'O')) win = true;
     if (game[1][0] == game[1][1] && game[1][1] == game[1][2] && (game[1][0] == 'X' || game[1][0] == 'O')) win = true;
     if (game[2][0] == game[2][1] && game[2][1] == game[2][2] && (game[2][0] == 'X' || game[2][0] == 'O')) win = true;
@@ -26,19 +30,24 @@ int main(){
                 cout << "Player 1: ";
             else
                 cout << "Player 2: ";
-            cout << "Which cell to mark? i:[1..3], j:[1..3]: "; 
+            cout << "Which cell to mark? i:[0..2], j:[0..2]: "; 
             cin >> i >> j;
             if (turn == false)
-               game[i][j] = 'X';
+               game[i][j] = player1;
             else 
-               game[i][j] = 'O';
+               game[i][j] = player2;
             if (isWin(game)){
                 cout << "Win!" << endl;
                 break; // need to terminate the problem
             }
         }
-    if (i==3) // all celles with i=0 to 2 have been inputted above but no winner yet
-       cout << "Tie!" << endl;
+    bool isNotFull = false;
+    for (i=0; i<3; i++){
+        for (j=0; j<3; j++)
+            if (game[i][j] == ' ') isNotFull = true;
+    }
+    if (isNotFull)
+        cout << "Tie!" << endl;
 
     // show the game to console
     cout << game[0][0] << " " << game[0][1] << " " << game[0][2] << endl;
